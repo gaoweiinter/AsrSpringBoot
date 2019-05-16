e3a18a363a71fc567b583c2ff4d562603aae74fe
moxuemeivip <moxuemeivip@outlook.com>
2019-05-16 11:01:23 +0800

    1.enable Erueka Client for this service 2.update CROS withCredentials

--- src/main/java/com/demo/richfit/asr/configuration/CorsConfig.java
+++ src/main/java/com/demo/richfit/asr/configuration/CorsConfig.java
@@ -17,9 +17,15 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
 	private CorsConfiguration buildConfig() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.addAllowedOrigin("*"); // 允许任何域名使用
+		
+		// addAllowedOrigin 不能设置为* 因为与 allowCredential 冲突,需要设置为具体前端开发地址
+		corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 允许任何域名使用
+		
 		corsConfiguration.addAllowedHeader("*"); // 允许任何头
 		corsConfiguration.addAllowedMethod("*"); // 允许任何方法（post、get等）
+		
+		// allowCredential 需设置为true
+        corsConfiguration.setAllowCredentials(true);
 		return corsConfiguration;
 	}
 
